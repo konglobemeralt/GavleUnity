b0637b50066566b1bd7dafca20b0d694ac0ea726
konglobemeralt <jesper.blidkvist@live.se>
2016-10-03 09:49:05 +0200

    FIXED terrain. For some reason builds empty webGL, works if imported into new project. Added some roads

---
+++ Assets/Scripts/mouseMain.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseMain : MonoBehaviour {
+    public Camera cameraFPS;
+    public Camera cameraBird;
+
+   public GameObject selectedObject;
+
+   public Shader shaderStandard;
+   public Shader shaderOutline;
+
+    void start()
+    {
+        cameraFPS = GameObject.Find("cameraFPS").GetComponent<Camera>();
+        cameraBird = GameObject.Find("cameraBird").GetComponent<Camera>();
+
+        shaderStandard = Shader.Find("Diffuse");
+        shaderOutline = Shader.Find("Outlined/Silhouetted Diffuse");
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+
+            bool hit;
+
+            if (cameraFPS.isActiveAndEnabled)
+            {
+                hit = Physics.Raycast(cameraFPS.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            }
+            else
+            {
+                hit = Physics.Raycast(cameraBird.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            }
+            
+            if (hit)
+            {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                if (hitInfo.transform.gameObject.tag == "Houses")
+                {
+                    selectObject(hitInfo.transform.gameObject);
+                    Debug.Log("It's working!");
+                }
+                else {
+                    Debug.Log("nopz");
+                    
+                }
+            }
+            else {
+                Debug.Log("No hit");
+                clearSelection();
+            }
+   
+        }
+    }
+
+
+    void selectObject(GameObject obj)
+    {
+        
+        if (selectedObject != null)
+        {
+            if (obj == selectedObject)
+                return;
+
+
+            clearSelection();
+
+        }
+        selectedObject = obj;
+
+        Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            r.material.shader = shaderOutline;
+        }
+    }
+
+    void clearSelection()
+    {
+
+        Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            r.material.shader = shaderStandard;
+        }
+        selectedObject = null;
+    }
+}
